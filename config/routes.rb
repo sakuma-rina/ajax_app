2287e0bf97580f8521197791373ed3a1ec12192c
sakuma-rina <aa3158148@gmail.com>
2020-09-16 16:16:03 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'messages/index'
-  root to: "rooms#index"
-  resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create, :destroy] do
-    resources :messages, only: [:index, :create]
-  end
+  root to: 'posts#index'
+  post 'posts', to: 'posts#create'
 end
 
-# アクションプラン
-# ①デバイスを導入し直す
-# ②% rails db:drop
-# ③% rails db:create
-# ④% rails db:migrate
-# ⑤サーバー再起動
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_103625) do
+ActiveRecord::Schema.define(version: 2020_09_16_031728) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2020_09_09_103625) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.boolean "checked"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
